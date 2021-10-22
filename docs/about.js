383155477458304f775278ff1ae0f238e0beebea
Emily-Levi <eul85@psu.edu>
2021-10-22 01:53:18 -0400

    Javascript assignment very broken :(

---
+++ docs/about.js
@@ -0,0 +1,20 @@
+sfunction changeColor(color) {
+                document.body.style.background = color;
+            }
+              
+            function Light() {
+                changeColor('white');
+                el_down.innerHTML = "Background Color changed";
+            }  
+            
+            
+           
+          
+            function changeColor(color) {
+                document.body.style.background = color;
+            }
+              
+            function Dark() {
+                changeColor('black');
+                el_down.innerHTML = "Background Color changed";
+            } 
