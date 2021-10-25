e156fab0b9f747d60342556b9b2a0f92de40ae89
Emily-Levi <eul85@psu.edu>
2021-10-25 13:22:19 -0400

    Javascript excerxise 3 is fixed :)

--- docs/about.js
+++ docs/about.js
@@ -1,20 +1,24 @@
-sfunction changeColor(color) {
-                document.body.style.background = color;
-            }
-              
-            function Light() {
-                changeColor('white');
-                el_down.innerHTML = "Background Color changed";
-            }  
-            
-            
-           
-          
-            function changeColor(color) {
-                document.body.style.background = color;
-            }
-              
-            function Dark() {
-                changeColor('black');
-                el_down.innerHTML = "Background Color changed";
-            } 
+window.addEventListener('DOMContentLoaded',init,false);
+
+
+function changeColor(color) {
+    document.body.style.background = color
+}
+
+function Light() {
+    changeColor('white');
+    el_down.innerHTML = "Background Color changed";
+}
+
+function Dark() {
+    changeColor('black');
+    el_down.innerHTML = "Background Color changed";
+}
+
+
+
+function init(){
+    var buttons = document.getElementsByTagName("button");
+    buttons[0].addEventListener("click", Light, false)
+    buttons[1].addEventListener("click", Dark, false)
+}
